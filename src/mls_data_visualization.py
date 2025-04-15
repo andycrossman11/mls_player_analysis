ec6ce559afac37e4ef3a59a22259056e7dd13770
Andy Crossman <andycrossman712@gmail.com>
2025-04-15 19:47:33 -0400

    added author note

--- src/mls_data_visualization.py
+++ src/mls_data_visualization.py
@@ -12,6 +12,7 @@ st.set_page_config(page_title="MLS Player Valuation Dashboard", layout="wide")
 st.title("⚽ MLS Player Valuation Dashboard")
 st.markdown(
     """
+    ##### Developed by AJC
     - A 2024 MLS player dataset was split by position group - **Defender, Midfielder, Forward**.
     - Within each position, the player value formula is based on a combination of **player efficiency** and **potential**.
     - The formula is designed to provide a comprehensive view of a player's current performance and future potential **relative to their position group.**
